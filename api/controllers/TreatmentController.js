2c8d3db2d1a45dcac480a6a1f8806eb6988c1e34
Segun <segunadeponle@gmail.com>
2018-01-20 17:27:18 +0100

    Create Treatment model

---
+++ api/controllers/TreatmentController.js
@@ -0,0 +1,11 @@
+/**
+ * TreatmentController
+ *
+ * @description :: Server-side logic for managing Treatments
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Treatment.js
@@ -0,0 +1,23 @@
+/**
+ * Treatment.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    name: {
+        type: 'string',
+    },
+    age: {
+        type: 'string',
+    },
+    gender: {
+        type: 'string',
+    },
+
+  }
+};
+
