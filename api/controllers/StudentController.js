a9cb410f70e909b484e1550078505dbb6e1aef9b
Segun <segunadeponle@gmail.com>
2018-02-18 14:57:43 +0100

    Implement crud operations for student bio data

--- api/controllers/StudentController.js
+++ api/controllers/StudentController.js
@@ -7,14 +7,36 @@
 
 module.exports = {
   create(req, res) {
-    req.body.owner = req.user.uniqueId;
-    Student.create(req.body)
-      .then((student) => {
-        res.ok({ student });
-      })
-      .catch((err) => {
-        res.notFound({ err });
-      });
+    const { uniqueId } = req.user;
+    User.findOne({
+      uniqueId,
+    }).then((result) => {
+      req.body.owner = result.id;
+      return Student.create(req.body);
+    }).then((student) => {
+      res.ok(student);
+    }).catch((err) => {
+      res.notFound({ err });
+    });
+  },
+  findStudentByuniqueId(req, res) {
+    const { uniqueId } = req.user;
+    User.findOne({
+      uniqueId,
+    }).then((result) => {
+      const owner = result.id;
+      return Student.findOne({
+        owner,
+      }).populate('owner');
+    }).then((student) => {
+      if (!student) {
+        res.notFound();
+        return;
+      }
+      res.ok(student);
+    }).catch((err) => {
+      res.notFound({ err });
+    });
   },
 };
 
--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,16 +9,20 @@
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
 
-module.exports.bootstrap = function(cb) {
+module.exports.bootstrap = function (cb) {
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  User.create({
-    uniqueId: '120407005',
-    password: 'segun',
-    type: 'Student',
-  }).then((e) => {});
 
+  User.count().then((count) => {
+    if (count == 0) {
+      return User.create({
+        uniqueId: '120407005',
+        password: 'segun',
+        type: 'Student',
+      });
+    }
+  }).then((e) => { });
 
   cb();
 };
